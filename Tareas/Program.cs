42337ae2c59f6fa9e287d05ea62ad0f0865b9d41
Juan Cruz <juangomezcorbalan@gmail.com>
2026-06-26 20:09:56 -0300

    terminado el punto 1

---
+++ Tareas/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using Tareas;
+using System.Linq;
+HttpClient cliente=new HttpClient();
+
+HttpResponseMessage respuesta=await cliente.GetAsync(@"https://jsonplaceholder.typicode.com/todos/");
+respuesta.EnsureSuccessStatusCode();
+
+string responseBody=await respuesta.Content.ReadAsStringAsync();
+List<Tarea> ?listaTareas=JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+Console.WriteLine("----------Tareas----------");
+// foreach(Tarea miTar in listaTareas)
+// {
+//     Console.WriteLine($"--Titulo:{miTar.title}");
+//     Console.WriteLine($"Completada:{miTar.completed}");
+// }
+
+List<Tarea> tareasPorEstado=listaTareas.OrderBy(t => t.completed).ToList();
+
+var opciones = new JsonSerializerOptions { WriteIndented = true };  //Para que el json este identado y sea legible
+string jsonString=JsonSerializer.Serialize(tareasPorEstado,opciones);
+await File.WriteAllTextAsync("tareas.json",jsonString);
